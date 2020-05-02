2f3736fa1a0b985d08ab9285a449c09d520d54eb
mporra27 <matoporramatias@gmail.com>
2020-05-02 17:04:20 -0300

    antes de la tarea de implementar detalles en tarjetas grandes

--- lib/src/wigets/ard_swiper_widget.dart
+++ lib/src/wigets/ard_swiper_widget.dart
@@ -19,14 +19,16 @@ class CardSwiper extends StatelessWidget {
         layout: SwiperLayout.STACK,
         itemWidth: 200.0,
         itemBuilder: (BuildContext context, int index) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              placeholder: AssetImage('assets/no-image.jpg'),
-              fit: BoxFit.cover,
-              image: NetworkImage( peliculas[index].getPosterImg()),
+          return Hero(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                fit: BoxFit.cover,
+                image: NetworkImage( peliculas[index].getPosterImg()),
 
-            )
+              )
+            ), tag: peliculas[index].id,
           );
         },
         itemCount: peliculas.length,
